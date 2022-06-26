085876eb00010f88db69c013025d6f3f8086cc2b
guoxiang <mzygdeaq@163.com>
2022-06-26 21:14:48 +0800

    feat: first commit

--- src/rm/rm.h
+++ src/rm/rm.h
@@ -47,4 +47,60 @@ private:
 
 std::ostream &operator<<(std::ostream &os, const bitmap &b);
 
+
+#define RM_PAGE_LIST_END      (-1)  // end of list of free pages
+#define RM_PAGE_FULLY_USED    (-2)  // page is fully used with no free slots
+
+struct RM_PageHdr
+{
+    // TODO:
+};
+
+//
+// RM_Record: RM Record interface
+//
+class RM_Record
+{
+    friend class RM_RecordTest;
+
+public:
+    RM_Record();
+    ~RM_Record();
+
+    // Return the data corresponding to the record.  Sets *pData to the
+    // record contents.
+    RC GetData(char *&pData) const;
+
+    // Sets data in the record for a fixed recordsize of size.
+    // Real object is only available and usable at this point not after
+    // construction
+    RC Set(char *pData, int size, RID id);
+
+    // Return the RID associated with the record
+    RC GetRid(RID &rid) const;
+
+private:
+    char *data_;
+    int record_size_;
+    RID rid_;
+};
+
+class RM_FileHandle
+{
+    // friend class RM_FileHandleTest;
+    friend class RM_FileScan;
+    friend class RM_Manager;
+public:
+
+private:
+};
+
+class RM_FileScan
+{
+};
+
+class RM_Manager
+{
+};
+
 #endif
